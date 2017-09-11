4dd6f54af6036515874997b5e1d8999141ff5336
kaushik vira <kaushik.vira@gmail.com>
2017-09-10 17:22:33 -0700

    My Init Commit

---
+++ smartapps/vira/announce-family-member-arrival.src/announce-family-member-arrival.groovy
@@ -0,0 +1,50 @@
+/**
+ *  Announce Family Member Arrival
+ *
+ *  Copyright 2017 kaushik vira
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ */
+definition(
+    name: "Announce Family Member Arrival",
+    namespace: "vira",
+    author: "kaushik vira",
+    description: "Announce Family member Arrival (For fun)",
+    category: "My Apps",
+    iconUrl: "http://www.gaiansolutions.com/img/icons/announcements-management.png",
+    iconX2Url: "http://www.gaiansolutions.com/img/icons/announcements-management.png",
+    iconX3Url: "http://www.gaiansolutions.com/img/icons/announcements-management.png")
+
+
+preferences {
+	section("Title") {
+		// TODO: put inputs here
+	}
+}
+
+def installed() {
+	log.debug "Installed with settings: ${settings}"
+
+	initialize()
+}
+
+def updated() {
+	log.debug "Updated with settings: ${settings}"
+
+	unsubscribe()
+	initialize()
+}
+
+def initialize() {
+	// TODO: subscribe to attributes, devices, locations, etc.
+}
+
+// TODO: implement event handlers
